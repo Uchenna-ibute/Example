b356ab84fa5e8b43a9db14af64b29c5fb657a114
Uchenna-ibute <77361109+Uchenna-ibute@users.noreply.github.com>
2022-07-19 02:11:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const navag = `
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Home</a></li>
+`
+const main = document.createElement("nav")
+nav.classlist.add("navigate")
+const side = document.createElement("ul")
+ul.innerHtml= navag
+document.querySelector("h1").append(main)
